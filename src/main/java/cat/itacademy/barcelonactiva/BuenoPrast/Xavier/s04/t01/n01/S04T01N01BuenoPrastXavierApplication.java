5c99a83e9910e1741af9e08278e53b546d801d32
XaviBueno <xbuenop@outlook.es>
2022-11-28 12:13:55 +0100

    Entregat

---
+++ src/main/java/cat/itacademy/barcelonactiva/BuenoPrast/Xavier/s04/t01/n01/S04T01N01BuenoPrastXavierApplication.java
@@ -0,0 +1,14 @@
+package cat.itacademy.barcelonactiva.BuenoPrast.Xavier.s04.t01.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01BuenoPrastXavierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01BuenoPrastXavierApplication.class, args);
+		System.out.println("Comença aplicació ");
+	}
+
+}
